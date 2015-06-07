681fe2b6b960386c01a3c985393d9a50f3763c7d
Hippo14 <hippo14@gmail.com>
2015-06-07 18:24:06 +0200

    Hello World!

---
+++ app/src/main/java/pz2015/habits/semestralny_l/AppConfig.java
@@ -0,0 +1,14 @@
+package pz2015.habits.semestralny_l;
+
+/**
+ * Created by ASUS on 2015-06-07.
+ */
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "";
+
+    // Server user registration url
+    public static String URL_REGISTER = "";
+
+}
---
+++ app/src/main/java/pz2015/habits/semestralny_l/SessionManager.java
@@ -0,0 +1,9 @@
+package pz2015.habits.semestralny_l;
+
+/**
+ * Created by ASUS on 2015-06-07.
+ */
+public class SessionManager {
+
+    
+}
